e17adccda163cafde47fcf098a38375dcca49db2
priyasamm <146506276+priyasamm@users.noreply.github.com>
2026-09-03 18:29:39 +0530

    Add files via upload

---
+++ datatype1.js
@@ -0,0 +1,10 @@
+let firstName="priya"
+console.log("The datatype of "+firstName+" is "+typeof firstName);
+let companyName="TCS"
+console.log("The datatype of "+companyName+" is "+typeof companyName);
+let mobilenumber=1819181918
+console.log("The datatype of "+mobilenumber+" is "+typeof mobilenumber);
+let isAutomation= true
+console.log("The datatype of "+isAutomation+" is "+typeof currentlyWorking);
+let hasPlaywright
+console.log("The datatype of "+hasPlaywright+" is "+typeof hasPlaywright);
