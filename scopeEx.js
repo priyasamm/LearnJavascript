64ebd1e0ddc20ea5a9556850434d9c56361657e4
priyasamm <146506276+priyasamm@users.noreply.github.com>
2026-09-03 18:26:16 +0530

    Add scopeEx.js to illustrate variable shadowing
    Demonstrate variable scope with browserVersion.

---
+++ scopeEx.js
@@ -0,0 +1,11 @@
+let browserVersion="chrome"
+function getBrowserVersion(){
+    if(browserVersion=="chrome"){
+        let browserVersion="google chrome"
+        console.log("inside if browserVersion: ",browserVersion);
+        
+    }
+    console.log("outside if browserVersion: ",browserVersion);
+}
+getBrowserVersion()
+console.log(browserVersion)
